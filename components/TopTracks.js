4b444018737547a80cb57fc94afd45c3fa20fc9f
Alberto LS <albertolopezsanchez9@gmail.com>
2020-12-24 10:13:48 +0100

    Refactorize TopTracks to use new UI/RankingList visual component

--- components/TopTracks.js
+++ components/TopTracks.js
@@ -1,61 +1,26 @@
 import useSWR from 'swr';
 import fetcher from '../lib/fetcher';
-import InfoCard from '../components/InfoCard';
-import styled from 'styled-components';
-
-const Title = styled.h3`
-  text-align: center;
-  font-size: 1.75rem;
-  font-weight: 500;
-`;
-
-const TopTracksWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  justify-content: center;
-
-  margin: 0 auto;
-
-  @media (min-width: 768px) {
-    max-width: 60vw;
-    flex-wrap: wrap;
-    flex-direction: row;
-  }
-`;
-
-const StyledInfoCard = styled.div`
-  display: flex;
-  justify-content: center;
-
-  margin-bottom: 0.5rem;
-
-  @media (min-width: 768px) {
-    margin: 0 0 1.5% 1.5%;
-    display: block;
-  }
-`;
+import RankingList from './UI/RankingList';
+import InfoCard from './UI/InfoCard';
 
 export default function TopTracks() {
   const { data } = useSWR('/api/top-tracks', fetcher);
 
   const trackList =
     data &&
-    data.tracks.map((track) => (
-      <StyledInfoCard key={track.songUrl}>
-        <InfoCard
-          imageUrl={track.albumImageUrl}
-          title={track.title || 'Playback stopped'}
-          subtitle={track.artist}
-          url={track.songUrl}
-        />
-      </StyledInfoCard>
+    data.tracks.map((track, index) => (
+      <InfoCard
+        key={index}
+        imageUrl={track.albumImageUrl}
+        title={track.title}
+        subtitle={track.artist}
+        url={track.songUrl}
+      />
     ));
 
   return (
     <>
-      <Title>Your top songs</Title>
-      <TopTracksWrapper>{trackList}</TopTracksWrapper>
+      <RankingList title="Your top songs">{trackList}</RankingList>
     </>
   );
 }
