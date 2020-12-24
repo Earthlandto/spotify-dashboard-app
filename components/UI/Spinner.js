95b237ef780213043b3f93954cdd931bd57b1026
Alberto LS <albertolopezsanchez9@gmail.com>
2020-12-24 11:52:07 +0100

    Create Spinner component

---
+++ components/UI/Spinner.js
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import styled from 'styled-components';
+
+const StyledSpinner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0.75rem;
+`;
+
+export default function Spinner({ size }) {
+  let width = 30;
+  switch (size) {
+    case 'medium':
+      width = 40;
+      break;
+    case 'large':
+      width = 74;
+      break;
+    default:
+      // small = 30;
+      break;
+  }
+  return (
+    <StyledSpinner>
+      <Image
+        src="/static/icons/kappa-spinner.svg"
+        height={width}
+        width={width}
+        alt="Loading"
+      />
+    </StyledSpinner>
+  );
+}
