6cb6c9d09af966876423f7ffa35a6545147669c9
Alberto LS <albertolopezsanchez9@gmail.com>
2020-12-24 09:53:12 +0100

    Allow changing limit of top tracks query

--- pages/api/top-tracks.js
+++ pages/api/top-tracks.js
@@ -1,10 +1,11 @@
 import { getTopTracks } from '../../lib/spotify';
 
 export default async (_, res) => {
-  const response = await getTopTracks();
+  const maxTracks = 10;
+  const response = await getTopTracks(maxTracks);
   const { items } = await response.json();
 
-  const tracks = items.slice(0, 10).map((track) => ({
+  const tracks = items.map((track) => ({
     artist: track.artists.map((_artist) => _artist.name).join(', '),
     songUrl: track.external_urls.spotify,
     title: track.name,
