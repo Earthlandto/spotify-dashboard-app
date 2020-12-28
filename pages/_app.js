de8c531f4c1406860fb51dfda56913cd42e3a828
Alberto LS <albertolopezsanchez9@gmail.com>
2020-12-28 10:21:21 +0100

    Add global config for SWR fetcher

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import '../styles/globals.css';
 import React from 'react';
 import Head from 'next/head';
 import AppHeader from '../components/AppHeader';
+import { SWRConfig } from 'swr';
+import fetcher from '../lib/fetcher';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -17,7 +19,16 @@ function MyApp({ Component, pageProps }) {
       </Head>
       <div>
         <AppHeader />
-        <Component {...pageProps} />
+        <SWRConfig
+          value={{
+            fetcher: fetcher,
+            revalidateOnFocus: false,
+            revalidateOnReconnect: false,
+            shouldRetryOnError: false,
+          }}
+        >
+          <Component {...pageProps} />
+        </SWRConfig>
       </div>
     </>
   );
