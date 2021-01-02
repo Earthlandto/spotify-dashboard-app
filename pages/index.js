383243bd99c7a75d634f68285d5cc435534cfb6e
Alberto LS <albertolopezsanchez9@gmail.com>
2021-01-02 17:12:38 +0100

    Change index page by showing a button to dashboard page

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,11 @@
 import React from 'react';
-import NowPlaying from '../components/NowPlaying';
-import TopTracks from '../components/TopTracks';
-import TopArtists from '../components/TopArtists';
-import Title from '../components/UI/Title.styled';
-import styled from 'styled-components';
+import Link from 'next/link';
 
-const StyledTitle = styled(Title)`
-  font-size: 1.8rem;
-  font-weight: bold;
-  text-align: center;
-
-  @media (min-width: 768px) {
-    font-size: 4rem;
-    font-weight: 500;
-  }
-`;
-
-const StyledRankings = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  @media (min-width: 768px) {
-    flex-direction: row;
-    justify-content: center;
-    min-width: 20rem;
-
-    & > * + * {
-      margin-left: 1.25rem;
-    }
-  }
-`;
-
-export default function Home() {
+export default function Index() {
   return (
     <div>
       <main>
-        <StyledTitle as="h1">
-          Your <strong>Spotify</strong> Dashboard
-        </StyledTitle>
-
-        <NowPlaying />
-
-        <StyledRankings>
-          <TopTracks />
-          <TopArtists />
-        </StyledRankings>
+        <Link href="/dashboard">Go to your dashboard</Link>
       </main>
     </div>
   );
