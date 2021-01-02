caa564c13d726b471c09a66277440c0f627cf939
Alberto LS <albertolopezsanchez9@gmail.com>
2021-01-02 17:18:49 +0100

    Show login button in index page and link to dashboard page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
+import LoginButton from '../components/LoginButton';
+import useCookie from 'react-use-cookie';
 
 export default function Index() {
+  const [userToken] = useCookie('token', '');
+  const Content = userToken ? (
+    <Link href="/dashboard">Go to your dashboard</Link>
+  ) : (
+    <LoginButton />
+  );
   return (
     <div>
-      <main>
-        <Link href="/dashboard">Go to your dashboard</Link>
-      </main>
+      <main>{Content}</main>
     </div>
   );
 }
