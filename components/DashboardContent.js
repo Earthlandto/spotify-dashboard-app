f8b039d80278458182f1bd425cda39176ab01ba7
Alberto LS <albertolopezsanchez9@gmail.com>
2021-01-02 17:09:15 +0100

    Add component with Dashboard content

---
+++ components/DashboardContent.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import NowPlaying from '../components/NowPlaying';
+import TopTracks from '../components/TopTracks';
+import TopArtists from '../components/TopArtists';
+import Title from '../components/UI/Title.styled';
+import styled from 'styled-components';
+
+const StyledTitle = styled(Title)`
+  font-size: 1.8rem;
+  font-weight: bold;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    font-size: 4rem;
+    font-weight: 500;
+  }
+`;
+
+const StyledRankings = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: center;
+    min-width: 20rem;
+
+    & > * + * {
+      margin-left: 1.25rem;
+    }
+  }
+`;
+
+export default function DashboardContent() {
+  return (
+    <>
+      <StyledTitle as="h1">
+        Your <strong>Spotify</strong> Dashboard
+      </StyledTitle>
+
+      <NowPlaying />
+
+      <StyledRankings>
+        <TopTracks />
+        <TopArtists />
+      </StyledRankings>
+    </>
+  );
+}
