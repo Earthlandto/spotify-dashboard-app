0d4d1c25f5daa08af3dbf16ee1ccf6e6f766fa14
Alberto LS <albertolopezsanchez9@gmail.com>
2021-01-02 17:11:33 +0100

    Add dashboard page with new DashboardContent component

---
+++ pages/dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import DashboardContent from '../components/DashboardContent';
+
+export default function Dashboard() {
+  return (
+    <main>
+      <DashboardContent />
+    </main>
+  );
+}
