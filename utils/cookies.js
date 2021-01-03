e29dc7002c2f18e5587aae1f4520c636ef698aaf
Alberto LS <albertolopezsanchez9@gmail.com>
2021-01-03 12:49:22 +0100

    Add getCookie util

--- utils/cookies.js
+++ utils/cookies.js
@@ -1,11 +1,16 @@
 import Cookies from 'universal-cookie';
 
-export const setCookie = (name = '', value, options) => {
+export const getCookie = (name = '', options = {}) => {
+  const cookies = new Cookies();
+  return cookies.get(name, options);
+};
+
+export const setCookie = (name = '', value, options = {}) => {
   const cookies = new Cookies();
   cookies.set(name, value, options);
 };
 
-export const removeCookie = (name = '', options) => {
+export const removeCookie = (name = '', options = {}) => {
   const cookies = new Cookies();
   cookies.remove(name, options);
 };
