cd240e8e89f807da83d50a235c98522376cf8f5f
Alberto LS <albertolopezsanchez9@gmail.com>
2021-01-03 12:16:57 +0100

    Add basic styles to index page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,69 @@
 import React from 'react';
 import Link from 'next/link';
 import LoginButton from '../components/LoginButton';
+import { Button } from '../components/UI/Button.styles';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpotify } from '@fortawesome/free-brands-svg-icons';
+import { faHeartbeat } from '@fortawesome/free-solid-svg-icons';
+
+const StyledMain = styled.main`
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  background-color: #333333;
+  background-image: linear-gradient(180deg, #333333 0%, #1db954 100%);
+`;
+
+const StyledInfo = styled.div`
+  max-width: 350px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+
+  padding: 1rem 0;
+
+  & + & {
+    padding-top: 0;
+  }
+
+  svg:nth-child(2) {
+    margin: 0 0.5rem;
+  }
+`;
 
 export default function Index() {
   const isSessionActive = useSelector((state) => state.isSessionActive);
+  const buttonStylesProps = {
+    color: 'white',
+    bgColor: 'transparent',
+  };
 
   const Content = isSessionActive ? (
-    <Link href="/dashboard">Go to your dashboard</Link>
+    <Link href="/dashboard">
+      <Button {...buttonStylesProps}>Go to your dashboard</Button>
+    </Link>
   ) : (
-    <LoginButton />
+    <LoginButton {...buttonStylesProps} />
   );
   return (
-    <div>
-      <main>{Content}</main>
-    </div>
+    <StyledMain>
+      <StyledInfo>
+        <FontAwesomeIcon icon={faSpotify} color="#1db954" size="5x" />
+        <FontAwesomeIcon icon={faHeartbeat} color="white" size="2x" />
+        <FontAwesomeIcon icon={faSpotify} color="#fa99cf" size="5x" />
+      </StyledInfo>
+      <StyledInfo as="p">
+        Connect your Spotify account to know more info about your activity in
+        Spotify
+      </StyledInfo>
+      {Content}
+    </StyledMain>
   );
 }
