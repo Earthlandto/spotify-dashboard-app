f84891a4d3936c079b3b12bb6ee3d00356c92f46
Alberto LS <albertolopezsanchez9@gmail.com>
2021-01-06 12:19:44 +0100

    Remove ok prop from api error format

--- utils/api-errors.js
+++ utils/api-errors.js
@@ -1,6 +1,5 @@
 /* Generated error format:
   {
-    ok: false
     status: <status_code>
     error: {
       message: <reason why the error happened>
@@ -13,13 +12,11 @@ export function createError(response, status, error = {}) {
   return response.status(status).json({
     status,
     error,
-    ok: false,
   });
 }
 
 export function createUnauthorizedError(response) {
   return response.status(401).json({
-    ok: false,
     status: 401,
     error: { message: 'Unauthorized: no token provided' },
   });
